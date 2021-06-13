f5377c51c8f61ba8466b4ecf5cbcce184e6e3e9f
Thomas Dimson <tdimson@gmail.com>
2021-06-13 15:50:19 -0700

    Final store owner address

--- js/packages/common/src/utils/ids.ts
+++ js/packages/common/src/utils/ids.ts
@@ -1,14 +1,14 @@
 import { PublicKey } from '@solana/web3.js';
 import { TokenSwapLayout, TokenSwapLayoutV1 } from '../models/tokenSwap';
 
-/*
 export const STORE_OWNER_ADDRESS = new PublicKey(
   'kickNLAj7N8kfEtXLuhpYJLGwZjJahuz7nr9tjKgn8e',
 );
-*/
+/*
 export const STORE_OWNER_ADDRESS = new PublicKey(
   'bckupPkvbcbG7aEdfCz8PMUcAHrhNxE2z5V5Wz3CdnV',
 );
+*/
 console.debug(`Store owner address: ${STORE_OWNER_ADDRESS?.toBase58()}`);
 
 export const WRAPPED_SOL_MINT = new PublicKey(
